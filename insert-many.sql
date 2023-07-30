5fe16f3fdc08ddb30a64245cb62fb1a3cc67297d
jrdnwlkr <walker.jordan.m@gmail.com>
2023-07-30 13:08:18 -0500

    initial sql set up

---
+++ insert-many.sql
@@ -0,0 +1,9 @@
+-- Insert multiple produce items --
+INSERT INTO inventory (id, name)
+VALUES
+    ( 1, "apple"),
+    ( 2, "orange"),
+    ( 3, "banana");
+    
+SELECT * FROM inventory;
+
---
+++ schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS shop_db;
+
+CREATE DATABASE shop_db;
+
+USE shop_db;
+
+CREATE TABLE inventory ( 
+    id INT,
+    name VARCHAR(100)
+)
