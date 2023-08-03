446d24bf43c6f4a9d52f611b2fe209172bc11424
jrdnwlkr <walker.jordan.m@gmail.com>
2023-08-03 12:29:48 -0500

    added CREATE TABLE for roles and departments

--- schema.sql
+++ schema.sql
@@ -4,7 +4,15 @@ CREATE DATABASE hr_db;
 
 USE hr_db;
 
-CREATE TABLE  ( 
-    id INT,
-    name VARCHAR(100)
-)
+CREATE TABLE departments ( 
+    id INT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE roles (
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+);
+
